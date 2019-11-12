01789e033e3f2ac86511778b86ee125e041c4388
londonFlynn <london.flynn@gmail.com>
2019-11-11 20:33:59 -0700

    Update WebSocketConfig.java

--- battleship/src/main/java/pro0/battleship/app/WebSocketConfig.java
+++ battleship/src/main/java/pro0/battleship/app/WebSocketConfig.java
@@ -1,4 +1,4 @@
-package pro0.battleship;
+package pro0.battleship.app;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
