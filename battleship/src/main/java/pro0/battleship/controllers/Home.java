1dcce341fcb8dc636037a3dd7fef9e2f9938627a
WhiteHatHacker1 <eonsahead@protonmail.com>
2019-11-13 19:32:03 -0700

    Pass loggedIn state to home.jsp and make Dock controller

--- battleship/src/main/java/pro0/battleship/controllers/Home.java
+++ battleship/src/main/java/pro0/battleship/controllers/Home.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,16 +17,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class Home {
 
 	@GetMapping(path="")
-	protected String doMainGet(Principal principal) {
-		String targetResource = "home";
-		if(principal != null)
-		{
-			targetResource = "loggedInHome";
-		}
+	protected String doMainGet(Principal principal, Model model) {
+		boolean loggedIn = false;
+		if(principal != null) loggedIn = true;
+		
+		model.addAttribute("loggedIn", loggedIn);
 		// For JS requests eventually
 //		((UserDetails) principal).getUsername()
 		
-		return targetResource;
+		return "home";
 	}
 
 }
