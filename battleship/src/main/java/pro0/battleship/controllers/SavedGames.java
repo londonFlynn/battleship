b80bc976854b41892b869e6fc9486f47395ec828
WhiteHatHacker1 <eonsahead@protonmail.com>
2019-11-13 18:46:31 -0700

    Replaced fetch user with test user

--- battleship/src/main/java/pro0/battleship/controllers/SavedGames.java
+++ battleship/src/main/java/pro0/battleship/controllers/SavedGames.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import pro0.battleship.models.Game;
 import pro0.battleship.models.User;
 import pro0.battleship.repositories.UserJpaRepository;
 
@@ -22,13 +23,15 @@ public class SavedGames {
 	@GetMapping(path="")
 	protected String doMainGet(Principal prn, Model model) {
 		String targetResource = "forward:/";
-		String username = ((UserDetails) prn).getUsername();
+//		String username = ((UserDetails) prn).getUsername();
 		
-		if(userRepo.findById(username).isPresent()) {
-			User user = userRepo.findById(username).get();
+//		if(userRepo.findById(username).isPresent()) {
+//			User user = userRepo.findById(username).get();
+			User user = new User("kathy", "test", "/hello", 0, 1);
+			user.getGames().add(new Game(user, user));
 			model.addAttribute("games", user.getGames());
 			targetResource = "savedGames";
-		}
+//		}
 		
 		return targetResource;
 	}
