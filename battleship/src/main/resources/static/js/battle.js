eb793e42962f0fc16dce1ed70b74dc1ffa36c8d1
Anna Ruiz <45431125+lilhappyburrito@users.noreply.github.com>
2019-12-02 18:51:18 -0700

    Update battle.js

--- battleship/src/main/resources/static/js/battle.js
+++ battleship/src/main/resources/static/js/battle.js
@@ -82,22 +82,23 @@ function playerMouseEvents() {
 		   for (let j = 0, col; col = row.cells[j]; j++) {
 			   if(col.id == targetId) {
 				   if(col.style.backgroundImage !='url("/images/placeShip.png")'){
-					   
 					   for(let k=1;k<placementAmount;k++){
 						   if(rotated){
-							   if((i+placementAmount-1) < 11) {
-								   col.style.backgroundImage="url(/images/target.gif)";
-								   playerBoard.rows[i+k].cells[j].style.backgroundImage="url(/images/target.gif)";							   
-							   }
-						   } else {						   
-							   if((j+placementAmount-1) < 11){
-								   try {
+							   if(playerBoard.rows[i+k+1].cells[j].style.background !='url("/images/placeShip.png")') {								   
+								   if((i+placementAmount-1) < 11) {
 									   col.style.backgroundImage="url(/images/target.gif)";
-									   row.cells[j+k].style.backgroundImage="url(/images/target.gif)";
+									   playerBoard.rows[i+k].cells[j].style.backgroundImage="url(/images/target.gif)";							   
 								   }
-								   catch(err) {
-//							   document.getElementById("demo").innerHTML = err.message;
-//								meh it's fine
+							   }
+						   } else {
+							   if(row.cells[j+k+1].style.backgroundImage!='url("/images/placeShip.png")') {								   
+								   if((j+placementAmount-1) < 11){
+									   try {
+										   col.style.backgroundImage="url(/images/target.gif)";
+										   row.cells[j+k].style.backgroundImage="url(/images/target.gif)";
+									   }
+									   catch(err) {
+									   }
 								   }
 							   }
 						   }
@@ -126,9 +127,13 @@ function playerMouseEvents() {
 							for(let k=1;k<placementAmount;k++){
 								try {
 									if(rotated) {
-										playerBoard.rows[i+k].cells[j].style.backgroundImage="url(/images/still.jpg)";
-									} else {							
-										row.cells[j+k].style.backgroundImage="url(/images/still.jpg)";
+										if(playerBoard.rows[i+k+1].cells[j].style.backgroundImage!='url("/images/placeShip.png")') {											
+											playerBoard.rows[i+k].cells[j].style.backgroundImage="url(/images/still.jpg)";
+										}
+									} else {
+										if(row.cells[j+k+1].style.backgroundImage!='url("/images/placeShip.png")') {
+											row.cells[j+k].style.backgroundImage="url(/images/still.jpg)";
+										}
 									}
 								}
 								catch(err) {
