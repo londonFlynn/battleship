cfd4161e74b61e0ec3bff4c21ca760ea10256eee
londonFlynn <47341087+londonFlynn@users.noreply.github.com>
2019-12-02 18:05:08 -0700

    refresh game over

--- battleship/src/main/resources/static/js/battle.js
+++ battleship/src/main/resources/static/js/battle.js
@@ -236,7 +236,7 @@ function setConnected(connected) {
     }
 }
 function notifyInvalidAttack() {
-    //TODO Notify user that they cannot attack that space
+
 }
 function userGotHit(position) {
 	position.xPos = position.xPos + "";
@@ -330,7 +330,6 @@ function userWon() {
     game.style.display = "none";
 }
 function itsYourTurn() {
-    //TODO unlock board
 	opponentBoard.style.display = "block";
 	var allOtiles = document.getElementsByClassName('otiles2');
 	document.getElementById("currentPlayerTurn").innerHTML = "Your Turn!";
@@ -366,7 +365,6 @@ function shipPlacementFailure(ShipType) {
 
 function displayShipInPositions(positions) {
     positions.forEach(function (position) {
-        //TODO show a ship in that position
     	position.xPos = position.xPos + "";
     	var initialAscii = position.xPos.charCodeAt(0) + 49;
     	var x = String.fromCharCode(initialAscii);
@@ -522,11 +520,14 @@ function reciveShipSunkNotificaiton(shipSunkNotification) {
     }
 }
 function reciveGameOverNotification(gameOverNotification) {
-    if (gameOverNotification.winner == username) {
-        userWon();
-    } else {
-        userLost();
-    }
+	if (gameOverNotification) {
+		console.log("game over");
+		if (gameOverNotification.winner == username) {
+			userWon();
+		} else {
+			userLost();
+		}
+	}
     // disconnect();
 }
 function reciveTurnChangeNotification(turnChangeNotification) {
@@ -550,7 +551,8 @@ function setupGameFromServer() {
     setupCellsFromServer();
     setupShipsFromServer();
 	setupDestroyedShipsFromServer();
-    setupGameHasStartedFromServer();
+	setupGameHasStartedFromServer();
+	setupGameHasEndedFromServer();
 }
 
 async function setupCellsFromServer() {
@@ -590,6 +592,9 @@ async function setupBoardRowFromServer(board, row, opponent) {
 
 function setupGameHasStartedFromServer() {
     sendRequest(null, "/gamestate/started/"+gameId, "GET", reciveGameStartedNotification);
+}
+function setupGameHasEndedFromServer() {
+    sendRequest(null, "/gamestate/over/"+gameId, "GET", reciveGameOverNotification);
 }
 
 function setupOpponentBoardFromServer() {
